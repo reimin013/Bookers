2164ca90775c5b274df1c7e5110269af7a3e0246
reimi <utatatan.013@gmail.com>
2020-03-31 19:35:37 +0900

    [Add] Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+  	@books = Book.all
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+      redirect_to book_path(@book)
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+end
